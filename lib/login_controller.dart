3d77790cd127b7439406a7d78f089dea9da28846
Vincent Goay <vincentgoay@gmail.com>
2021-08-02 10:16:24 +0800

    Implement login controller and mock auth service

---
+++ lib/login_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tuts_provider_consumer/auth_service.dart';
+
+class LoginController extends ChangeNotifier {
+  // Service should be of type Singleton (only one instance is created and used by the entire application)
+  // The example here is not showing a singleton but rather instantiate a new instance
+  // For simplicity sake, we will go with that
+  AuthService authService = AuthService();
+
+  // Initialize the starting state to be Initial
+  LoginState _state = LoginState.Initial;
+  //
+  LoginState get state => _state;
+
+  // Login with email method (event)
+  Future<void> loginWithEmail(
+      String username, String password, bool mockResult) async {
+    // First update state loading
+    _setState(LoginState.Loading);
+
+    bool result = await authService.loginWithEmail(mockResult);
+    result ? _setState(LoginState.Done) : _setState(LoginState.Error);
+  }
+
+  // Update current state and notify all reigstered listener
+  void _setState(LoginState state) {
+    _state = state;
+    notifyListeners();
+  }
+}
+
+// These are the states that a screen should handle
+enum LoginState { Initial, Loading, Done, Error }
